3e8c466c344a1320bf040db44941ae0193c77066
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-10-30 19:07:34 +0900

    init

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-
-
+from lib.br_1 import BR1
+from lib.br_2 import BR2
+from lib.br_3 import BR3
+from lib.br_4 import BR4
 
 def main():
-    pass
+    br1 = BR1("AOI")
+    print(br1.name)
 
 
 if __name__ == "__main__":
