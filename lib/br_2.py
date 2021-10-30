039a7170a3494fa7cd32944ab44c710fed1bd55d
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-10-30 19:16:59 +0900

    edit br_2

--- lib/br_2.py
+++ lib/br_2.py
@@ -1,8 +1,13 @@
 
 class BR2(object):
-    def __init__(self, name):
+    def __init__(self, name: str, gender: bool):
         self._name = name
+        self._gender = gender
     
     @property
     def name(self):
         return self._name
+    
+    @property
+    def gender(self):
+        return "Male" if self._gender == True else "Female"
