db4e22c841d5dac28f92af49ab6d8293fd4a4acd
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-10-30 19:25:56 +0900

    add major to BR3

--- lib/br_3.py
+++ lib/br_3.py
@@ -1,8 +1,13 @@
 
 class BR3(object):
-    def __init__(self, name):
+    def __init__(self, name: str, major: str):
         self._name = name
+        self._major = major
     
     @property
     def name(self):
         return self._name
+    
+    @property
+    def major(self):
+        return self._major
