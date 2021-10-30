d186b6b4b7b6e65e62b69749b647467c19ebebf1
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-10-30 19:11:05 +0900

    BR1クラスのプロパティ追加

--- lib/br_1.py
+++ lib/br_1.py
@@ -1,8 +1,14 @@
 
 class BR1(object):
-    def __init__(self, name):
+    def __init__(self, name: str, age: int):
         self._name = name
+        self._age = age
     
     @property
     def name(self):
         return self._name
+    
+    @property
+    def age(self):
+        return self._age
+    
